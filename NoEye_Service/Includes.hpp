96e19a3a6825f8b4dc6ef56112202c34b44d09cf
Schnocker <Schnocker@users.noreply.github.com>
2017-06-03 12:36:33 +0200

    Update Includes.hpp

--- NoEye_Service/Includes.hpp
+++ NoEye_Service/Includes.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#define VMPROTECT 1
 
 #include <Windows.h>
 #include <iostream>
@@ -9,14 +8,3 @@
 #include <time.h>
 #include <TlHelp32.h>
 
-#if (VMPROTECT == 1)
-#include <VirtualizerSDK/VirtualizerSDK.h>
-#ifdef _WIN64 
-#pragma comment(lib,"VirtualizerSDK64")
-#elif _WIN32
-#pragma comment(lib,"VirtualizerSDK32")
-#endif
-#else
-static void VirtualizerStart() {}
-static void VirtualizerEnd() {}
-#endif
